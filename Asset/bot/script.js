e93a81d50472622189794c7bca98859fa81949a4
MichaellIbarra <michaell.ibarra@vallegrande.edu.pe>
2023-08-09 17:01:44 -0500

    tree send

---
+++ Asset/bot/script.js
@@ -0,0 +1,49 @@
+document.getElementById("userInput").addEventListener("input", function () {
+    var characterCount = this.value.length;
+    document.getElementById("send-btn").disabled = characterCount < 3;
+});
+
+function extractDriveImageUrl(cellValue) { var imageRegex = /https:\/\/drive\.google\.com\/open\?id=([\w-]+)/; var videoRegex = /https:\/\/drive\.google\.com\/file\/d\/([\w-]+)\/view/; var imageMatches = cellValue.match(imageRegex); var videoMatches = cellValue.match(videoRegex); if (imageMatches && imageMatches.length > 1) { var imageId = imageMatches[1]; return `http://drive.google.com/uc?export=view&id=${imageId}`; } if (videoMatches && videoMatches.length > 1) { var videoId = videoMatches[1]; return `https://drive.google.com/file/d/${videoId}/preview`; } return ''; }
+
+var apiKey = 'AIzaSyAmCLwuc94_hiXPDcVsYWV7toMyPqsrot8', url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${hoja}?key=${apiKey}`;
+w9wqhwdigwq
+function enviarMensaje() {
+    var userInput = document.getElementById('userInput').value.toLowerCase().trim(); if (userInput === '') { return; }
+    var chatbox = document.getElementById('chatbox');
+    chatbox.innerHTML += '<div style="background-color:#00a884;" class="small p-2 ms-3 mb-3 rounded-3" role="alert">' + userInput + '</div>';
+    function comentario(mensaje) { chatbox.innerHTML += '<div style="background-color:#1DA1F2;" class="small p-2 ms-3 mb-3 rounded-3" role="alert">' + mensaje + '</div>'; document.getElementById('userInput').value = ''; }
+    const respuestas = {
+        "hola": "holii :)",
+        "como estás?": "bien y tu?",
+        "bien": "me alegro mucho!",
+        "mal": "pipipipip",
+        "para que es está página web": "un chat bot para enterarte todas las noticias del colegio"
+    };
+
+    if (respuestas.hasOwnProperty(userInput)) { comentario(respuestas[userInput]); return; }
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            var respuesta = '', resultadosEncontrados = false;
+            if (data.values && data.values.length > 0) {
+                for (var i = 1; i < data.values.length; i++) {
+                    var fila = data.values[i], titulo = fila[2].toLowerCase(), descripcion = fila[3].toLowerCase();
+                    if (titulo.includes(userInput) || descripcion.includes(userInput)) {
+                        var marcaTemporal = fila[0];
+                        var fotoOVideo = extractDriveImageUrl(fila[1]);
+                        var titulo = fila[2];
+                        var descripcion = fila[3];
+                        var categoria = fila[4];
+                        respuesta += `<p><strong>Fecha de la Noticia:</strong> ${marcaTemporal}</p><a href="${fotoOVideo}" target="_blank"><img style="margin-bottom: 10px;" width="300" height="200" src="${fotoOVideo}" alt="Dar Click"></a><p><strong>Título:</strong> ${titulo}</p><p><strong>Descripción:</strong> ${descripcion}</p><p><strong>Categoría:</strong> ${categoria}</p><hr>`;
+                        resultadosEncontrados = true;
+                    }
+                }
+            }
+            if (!resultadosEncontrados) {
+                respuesta = '<p>No se encontraron resultados.</p>';
+            }
+            chatbox.innerHTML += '<div style="background-color:#1DA1F2;" class="small p-2 ms-3 mb-3 rounded-3" role="alert">' + respuesta + '</div>';
+        })
+        .catch(error => console.error('Error al obtener los datos:', error));
+    document.getElementById('userInput').value = '';
+}
--- Asset/publicacion/script.js
+++ Asset/publicacion/script.js
@@ -1,6 +1,5 @@
 function extractDriveId(cellValue) { var regex = /https:\/\/drive\.google\.com\/file\/d\/([\w-]+)/; var matches = cellValue.match(regex); if (matches && matches.length > 1) { var imageId = matches[1]; return imageId; } return ''; }
 function extractDriveImageUrl(cellValue) { var regex = /https:\/\/drive\.google\.com\/file\/d\/([\w-]+)/; var matches = cellValue.match(regex); if (matches && matches.length > 1) { var imageId = matches[1]; return `https://drive.google.com/uc?export=view&id=${imageId}`; } return ''; }
-//Sistema de mostrar lo que contiene el google sheet
 var apiKey = 'AIzaSyAmCLwuc94_hiXPDcVsYWV7toMyPqsrot8', currentRow = 0, allMessagesVisible = false;
 const spreadsheetId = '1IO8BIE58F3D70EMkY-KIwl1A6sOw90fZTQwRcuzc9jc';
 function chatMostrar() { var chatMessages = document.querySelectorAll('.chat-message'); var toggleButton = document.getElementById('toggleButton'); if (!allMessagesVisible) { for (var i = currentRow + 4; i < chatMessages.length; i++) { chatMessages[i].style.display = 'none'; } toggleButton.textContent = 'Mostrar todos los mensajes'; } else { for (var i = currentRow + 4; i < chatMessages.length; i++) { chatMessages[i].style.display = 'flex'; } toggleButton.textContent = 'Solo los 4 primeros mensajes'; } allMessagesVisible = !allMessagesVisible; }
@@ -15,13 +14,11 @@ function getDataFromSheets() {
                 var row = rows[currentRow];
                 var archivo = row[0];
                 var esVideo = archivo.includes("drive.google.com/file/d/");
-
                 if (esVideo) {
                     var videoId = extractDriveId(archivo);
                     var videoUrl = `https://drive.google.com/file/d/${videoId}/preview`;
                     var contentDiv = document.getElementById('content');
                     var iframe = document.createElement('iframe');
-
                     iframe.src = videoUrl;
                     iframe.width = 300;
                     iframe.height = 260;
@@ -33,22 +30,17 @@ function getDataFromSheets() {
                     iframe.mozallowfullscreen = false;
                     iframe.allow = "autoplay";
                     iframe.allowFullscreen = true;
-
                     contentDiv.appendChild(iframe);
-
                     var titulo = row[1];
                     var parrafo = row[2];
                     var nuevaColumna = '';
-
                     var div = document.createElement('div');
                     contentDiv.appendChild(div);
-
                     var div = document.createElement('div');
                     var h2 = document.createElement('h2');
                     h2.classList.add('heading');
                     h2.textContent = titulo;
                     div.appendChild(h2);
-
                     var p = document.createElement('p');
                     p.textContent = parrafo;
                     div.appendChild(p);
@@ -57,7 +49,6 @@ function getDataFromSheets() {
                     var imagenUrl = extractDriveImageUrl(archivo);
                     var contentDiv = document.getElementById('content');
                     var img = document.createElement('img');
-
                     img.src = imagenUrl;
                     img.width = 360;
                     img.height = 200;
@@ -65,13 +56,10 @@ function getDataFromSheets() {
                     img.draggable = "false";
                     img.style.display = "block";
                     img.style.margin = "0 auto";
-
                     contentDiv.appendChild(img);
-
                     var titulo = row[1];
                     var parrafo = row[2];
                     var nuevaColumna = '';
-
                     var div = document.createElement('div');
                     contentDiv.appendChild(div);
                 }
@@ -79,48 +67,37 @@ function getDataFromSheets() {
                 for (var j = 3; j < row.length; j++) {
                     if (row[j].trim() !== '') {
                         var mensaje = row[j].replace(/\n+/g, ' ');
-
                         if (mensaje.trim() !== '') {
                             nuevaColumna += mensaje + '\n';
                         }
                     }
                 }
-
                 var chatContainer = document.createElement('div');
                 chatContainer.classList.add('chat-container');
-
                 var mensajesSeparados = nuevaColumna.split('\n');
-
                 for (var j = 0; j < mensajesSeparados.length; j++) {
                     var mensaje = mensajesSeparados[j];
-
                     if (mensaje.trim() !== '') {
                         var chatMessage = document.createElement('div');
                         chatMessage.classList.add('chat-message');
-
                         var chatContent = document.createElement('div');
                         chatContent.classList.add('chat-content');
-
                         var strong = document.createElement('strong');
                         strong.textContent = 'Alumn@: ' + mensaje;
-
                         chatContent.appendChild(strong);
                         chatMessage.appendChild(chatContent);
                         chatContainer.appendChild(chatMessage);
                     }
                 }
-
                 contentDiv.appendChild(chatContainer);
                 OcultarTodo();
             } else {
                 console.log('Error: No puedo encontrar los datos. Contactar con Michaell.');
             }
-
         })
         .catch(error => console.error('Error al obtener los datos:', error));
 }
 getDataFromSheets();
-// Sistema de enviar dato al google sheet
 const form = document.forms['contactform'], botonEnviar = document.getElementById("submit"), scriptURL = 'https://script.google.com/macros/s/AKfycbyAxUmWk-lNDrdHm8Yw24Q5MhpSRB_Aw3hFzMtu8rBlq8m7TtZVEd1Y4vEdlXBbgYZiHQ/exec', messageContainer = document.getElementById('messageContainer');
 form.addEventListener('submit', async e => {
     e.preventDefault();
@@ -151,27 +128,21 @@ form.addEventListener('submit', async e => {
         botonEnviar.disabled = false;
     }
 });
-
 const celdaA = 'A2';
 const celdaB = 'B2';
-
 document.addEventListener("DOMContentLoaded", function () {
     const botonIncrementarA = document.getElementById("botonIncrementarA");
     const botonIncrementarB = document.getElementById("botonIncrementarB");
     const messageContainer = document.getElementById("messageContainer");
-
     botonIncrementarA.addEventListener("click", function () {
         enviarSolicitud(celdaA, messageContainer, botonIncrementarA);
     });
-
     botonIncrementarB.addEventListener("click", function () {
         enviarSolicitud(celdaB, messageContainer, botonIncrementarB);
     });
 });
-
 function enviarSolicitud(celda, messageContainer, boton) {
     boton.disabled = true;
-
     fetch(`https://script.google.com/macros/s/AKfycbzYKrTnh5Rnc7qJ_RLRMIJBGvvBECDouYfW0UvVzLGoXheoRWJfMbqRbhP4UL2WkkDL9A/exec?hoja=${hoja}&celda=${celda}`, { method: "POST" })
         .then(response => response.text())
         .then(data => {
@@ -187,7 +158,6 @@ function enviarSolicitud(celda, messageContainer, boton) {
             }, 10000);
         });
 }
-
 function mostrarMensajeExitoso(messageContainer) {
     const parrafo = document.createElement("p");
     parrafo.textContent = "Fue enviado con éxito. Recargue la página";
@@ -201,8 +171,6 @@ function mostrarMensajeExitoso(messageContainer) {
         messageContainer.innerHTML = '';
     }, 6000);
 }
-
-
 function getDataFromSheetss() {
     var url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${hoja}?key=${apiKey}`;
     fetch(url)
@@ -212,8 +180,6 @@ function getDataFromSheetss() {
             const valorB1 = data.values[1][1];
             const valorCeldaElemento = document.getElementById("valorCelda");
             const valorCeldaElementoo = document.getElementById("valorCelda2");
-
-            // Verificar si el valor es undefined y asignar 0 en su lugar
             valorCeldaElemento.textContent = valorA1 !== undefined ? valorA1 : 0;
             valorCeldaElementoo.textContent = valorB1 !== undefined ? valorB1 : 0;
         })
@@ -221,6 +187,4 @@ function getDataFromSheetss() {
             console.error("Error al obtener los datos de Google Sheets:", error);
         });
 }
-
-
 getDataFromSheetss();
